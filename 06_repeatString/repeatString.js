51546c1b9a5796f53cfbb33c9c70d5be2967c9f7
ignacio <ignaciogabrielleguizamon@gmail.com>
2025-08-19 23:27:48 -0300

    exercise 6 to 11 finished

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let result = '';
+  if (num >= 0){
+     for (let i = 0; i < num ; i++) {
+    result +=  string;
+  }
+    return result
+  } else{
+    return "ERROR"
+  }
+ 
+ 
 };
+
+console.log(repeatString("hey", 3))
 
 // Do not edit below this line
 module.exports = repeatString;
